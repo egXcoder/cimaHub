92988b71d703542f5ee1a1e25c33e8239785a544
Your Name <you@example.com>
2019-04-05 14:28:09 +0100

    added

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,1 +1,91 @@
-{{-- your code goes here --}}
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+
+    <head> 
+        <meta charset="UTF-8">
+        <meta name="description" content="أفضل موقع عربي لمشاهدة الأفلام و المسلسلات و الأنمي بسهولة و سرعة">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Movie</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/shared.css">
+        <link rel="stylesheet" href="css/single.css">
+
+
+    </head>
+
+    <body>
+        
+        
+        <header class="main-header">
+
+            <div class="navbar-container">
+                <div class="toggle-button">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <nav class="main-header__navbar">
+                    <ul>
+                        <li><a href="#">الرئيسية</a></li>
+                        <li><a href="أفلام عربي/index.html">أفلام عربي</a></li>
+                        <li><a href="#">أفلام أجنبي</a></li>
+                        <li><a href="#">مسلسلات عربي</a></li>
+                        <li><a href="#">مسلسلات أجنبي</a></li>
+                        <li><a href="#">أنمي</a></li>
+                        
+                    </ul>
+                </nav>
+            </div>
+            
+            
+            <div class="logo"><i class="fas fa-tv"></i>
+                <h3><span style="font-size: 130%" >C</span>inemaHub</h3>
+            </div>
+            
+            
+
+        </header>
+        
+        <div id="movie-cover">
+            <div class="movie-poster">
+                
+            
+            </div>
+        
+        </div>
+
+        
+        
+        
+        <div id="search-bar">
+            <span><i class="fas fa-search"></i></span>
+            <input class="search-bar__input" placeholder="بحث..."></input>
+        </div>
+
+        <main id="container">
+        
+            <div id="main">   
+
+                
+        
+    </main>
+    
+        
+
+        <footer id="main-footer">
+            <div class="go-to-top">
+                <i class="fas fa-arrow-up"></i>
+            </div>
+            
+
+        </footer>
+    
+    
+    
+    
+</body>
+    
+    
+</html>

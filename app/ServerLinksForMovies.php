eb9da7d7ab923e9ddc2ab7187a179a7a4c203f37
Ahmed <ah.ib94@hotmail.com>
2019-04-05 15:57:58 +0200

    added more data to seeder

--- app/ServerLinksForMovies.php
+++ app/ServerLinksForMovies.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ServerLinksMovies extends Model
+class ServerLinksForMovies extends Model
 {
     protected $table = 'server_links_movies';
     public $timestamps = false;
